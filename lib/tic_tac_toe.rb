b3384145669939a924ec2eaf6c1865c8923aebf8
Iain McKenzie <iainmck29@hotmail.com>
2018-09-27 11:13:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ def valid_move?(board, position)
  input = gets.strip
  index = input_to_index(input)
  if valid_move?(board, index) == true
-   move(board, index, character = "X")
+   player_move(board, index, character = "X")
    display_board(board)
  else turn(board)
  end
