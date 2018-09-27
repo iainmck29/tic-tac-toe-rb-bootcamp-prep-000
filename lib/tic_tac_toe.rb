800cb2e2de787cf748fa6c80a9b9ea2dd34d0ebe
Iain McKenzie <iainmck29@hotmail.com>
2018-09-27 11:09:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,11 @@ end
 
 
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+end
 end
 
 def valid_move?(board, position)
